81b217ae395e904c32567e4f6aa7499bd8bd0331
moon <d.moon23k.gmail.com>
2022-05-17 17:40:45 +0900

    bug fixed

--- NMT/WMT14/download_data.sh
+++ NMT/WMT14/download_data.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-cd data
 mkdir -p wmt
 cd wmt
 
@@ -17,29 +15,29 @@ cd training && ls | grep -v 'europarl-v7.de-en.[de,en]' | xargs rm
 cd .. && mv training/europarl* . && rm -r training training-parallel-europarl-v7.tgz
 
 
+#concat train files
+cat commoncrawl.de-en.en europarl-v7.de-en.en > train_orig.en
+cat commoncrawl.de-en.de europarl-v7.de-en.de > train_orig.de
+
+#downsize train files
+awk '{ if (NR % 10 == 0) print $0; }' train_orig.en > train.en
+awk '{ if (NR % 10 == 0) print $0; }' train_orig.de > train.de
+rm train_orig.en train_orig.de
+
+
 #Getting Valid and Test datset
-wget --trust-server-names http://www.statmt.org/wmt14/test-filtered.tgz
-wget --trust-server-names http://data.statmt.org/wmt17/translation-task/test.tgz
-tar zxvf test-filtered.tgz && tar zxvf test.tgz
-cd test && ls | grep -v '.*deen\|.*ende' | xargs rm
-mv test/* . && rm -r test-filtered.tgz test.tgz test
+wget --trust-server-names https://www.statmt.org/wmt14/dev.tgz
+wget --trust-server-names https://www.statmt.org/wmt14/test-filtered.tgz
+tar zxvf dev.tgz && tar zxvf test-filtered.tgz
 
+mv dev/newstest2013.en valid.en
+mv dev/newstest2013.de valid.de
 
-#Concat common and euro datasets
-cat commoncrawl.de-en.en europarl-v7.de-en.en > train.en
-cat commoncrawl.de-en.de europarl-v7.de-en.de > train.de
-rm common* euro*
-
+mv test/newstest2014-deen-src.en.sgm .
+mv test/newstest2014-deen-src.de.sgm .
 
 wget -nc https://raw.githubusercontent.com/moses-smt/mosesdecoder/master/scripts/ems/support/input-from-sgm.perl
+perl input-from-sgm.perl < newstest2014-deen-src.en.sgm > test.en
+perl input-from-sgm.perl < newstest2014-deen-ref.de.sgm > test.de
 
-
-valid=newstest2014-deen
-test=newstest2017-ende
-
-perl input-from-sgm.perl < $valid-src.en.sgm > valid.en
-perl input-from-sgm.perl < $valid-ref.de.sgm > valid.de
-perl input-from-sgm.perl < $test-src.en.sgm > test.en
-perl input-from-sgm.perl < $test-ref.de.sgm > test.de
-
-rm news* input*
+rm -rf dev test *sgm *perl *tgz common* euro*
