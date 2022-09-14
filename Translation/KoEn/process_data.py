6e946bf5e189dc3889d3b48b24d98b4f87986e65
moon <d.moon23k.gmail.com>
2022-09-14 18:14:19 +0900

    minor logic modified

--- Translation/KoEn/process_data.py
+++ Translation/KoEn/process_data.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 
 
@@ -6,14 +7,14 @@ d_names = ['1_구어체(1).xlsx', '1_구어체(2).xlsx', '2_대화체.xlsx'
            '3_문어체_뉴스(3).xlsx', '3_문어체_뉴스(4).xlsx']
 
 
+def read_data(data_name):
+    df = pd.read_excel(f'd_name')
+    return [{'src': src, 'trg': trg} for src, trg in zip(df.iloc[:, -2].values.tolist(), df.iloc[:, -1].values.tolist())]
+
+
 def main():
-    src, trg = [], []
-    for d_name in d_names:
-        df = pd.read_excel(f'd_name')
-        src.append(df.iloc[:, -2].values.tolist())
-        trg.append(df.iloc[:, -1].values.tolist())
-    
     return
+
 
 if __name__ == '__main__':
     main()
